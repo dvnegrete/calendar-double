2bfdc7269bd24a38f6f986e62ab26ba89b4372be
Damian V.N <dvnegrete@gmail.com>
2024-01-26 21:49:51 -0600

    Unit test a <header-calendar/> coverage al 100%

--- calendar-double/src/components/header-calendar/header-calendar.tsx
+++ calendar-double/src/components/header-calendar/header-calendar.tsx
@@ -14,32 +14,44 @@ export class HeaderCalendar {
   @Event({ eventName:'dvn-nextMonthCalendar', composed:true, cancelable: true, bubbles: true }) nextMonthCalendar: EventEmitter;
   @Event({ eventName:'dvn-previousMonthCalendar', composed:true, cancelable: true, bubbles: true }) previousMonthCalendar: EventEmitter;
 
+  headerLeft(){
+    return (
+      <div 
+        class={this.nameInactive ? 'button-next hidden': 'button-next'} 
+        onClick={ ()=> this.previousMonthCalendar.emit() }>
+          <img 
+            src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/left-chevron.svg" 
+            alt="atras"
+          />
+      </div>
+    )
+  }
+
+  headerRight(){
+    return (
+      <div 
+        class={this.nameInactive ? 'button-next hidden': 'button-next'} 
+        onClick={ ()=> this.nextMonthCalendar.emit() }>
+          <img 
+          src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/right-chevron.svg" 
+          alt="siguiente" />
+      </div>
+    );
+  }
+
+  headerEmpty(){
+    return (
+    <div class='empty'></div>
+    );
+  }
+
   render() {
     return (
-      <header>
-        {this.twoArrow || this.position === 'left' ? 
-        <div 
-          class={this.nameInactive ? 'button-next hidden': 'button-next'} 
-          onClick={ ()=> this.previousMonthCalendar.emit() }>
-            <img 
-              src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/left-chevron.svg" 
-              alt="atras"
-            />
-        </div>
-        : <div class='empty'></div>
-        }
-        <span class={ this.nameInactive ? 'inactive' : '' }>{ this.nameMonth } { this.year }</span> 
-        { this.twoArrow || this.position === 'right' ? 
-          <div 
-            class={this.nameInactive ? 'button-next hidden': 'button-next'} 
-            onClick={ ()=> this.nextMonthCalendar.emit() }>
-              <img 
-              src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/right-chevron.svg" 
-              alt="siguiente" />
-          </div>
-        : <div class='empty'></div>
-        }
-      </header>
+        <header>
+          { this.twoArrow || this.position === 'left' ? this.headerLeft() : this.headerEmpty() }
+          <span class={ this.nameInactive ? 'inactive' : '' }>{ this.nameMonth } { this.year }</span> 
+          { this.twoArrow || this.position === 'right' ? this.headerRight() : this.headerEmpty() }
+        </header>
     );
   }
 
--- calendar-double/src/components/header-calendar/test/header-calendar.spec.tsx
+++ calendar-double/src/components/header-calendar/test/header-calendar.spec.tsx
@@ -1,7 +1,9 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { HeaderCalendar } from '../header-calendar';
+import { h } from '@stencil/core';
 
 describe('header-calendar', () => {
+    
   it('renders', async () => {
     const page = await newSpecPage({
       components: [HeaderCalendar],
@@ -10,9 +12,94 @@ describe('header-calendar', () => {
     expect(page.root).toEqualHtml(`
       <header-calendar>
         <mock:shadow-root>
-          <slot></slot>
+          <header>
+            <div class="empty"></div>
+            <span></span>
+            <div class="empty"></div>
+          </header>
         </mock:shadow-root>
       </header-calendar>
     `);
   });
+  
+  it('render with twoArrow=true', async () => {
+    const page = await newSpecPage({
+      components: [HeaderCalendar],
+      template: ()=> (<header-calendar two-arrow="true"/>),
+    });
+    console.log(page.rootInstance);
+    expect(page.root).toEqualHtml(`
+    <header-calendar two-arrow="true">
+        <mock:shadow-root>
+          <header>
+            <div class="button-next">
+              <img 
+                src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/left-chevron.svg" 
+                alt="atras"
+              />
+            </div>
+            <span></span>
+              <div class="button-next">
+                <img 
+                src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/right-chevron.svg" 
+                alt="siguiente" />
+              </div>
+          </header>
+        </mock:shadow-root>
+      </header-calendar>
+    `);
+  });
+ 
+  it('render with twoArrow=true and nameInactive=true', async () => {
+    const page = await newSpecPage({
+      components: [HeaderCalendar],
+      template: ()=> (<header-calendar two-arrow="true" name-inactive="true"/>),
+    });
+    console.log(page.rootInstance);
+    expect(page.root).toEqualHtml(`
+    <header-calendar two-arrow="true" name-inactive="true">
+        <mock:shadow-root>
+          <header>
+            <div class="button-next hidden">
+              <img 
+                src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/left-chevron.svg" 
+                alt="atras"
+              />
+            </div>
+            <span class="inactive"></span>
+              <div class="button-next hidden">
+                <img 
+                src="https://github-personal-dvn.s3.us-east-2.amazonaws.com/img/right-chevron.svg" 
+                alt="siguiente" />
+              </div>
+          </header>
+        </mock:shadow-root>
+      </header-calendar>
+    `);
+  });
+
+  it('should events capture previousMonthCalendar', async () => {
+    const page = await newSpecPage({
+      components: [HeaderCalendar],
+      template: ()=> (<header-calendar two-arrow="true"/>),
+    });
+    const component = page.rootInstance as HeaderCalendar;
+    const emitSpy = jest.spyOn(component.previousMonthCalendar, 'emit');
+    const button = page.root.shadowRoot.querySelector('.button-next') as HTMLElement;
+    button.click();
+    expect(emitSpy).toHaveBeenCalled();
+  });
+  
+  it('should events capture nextMonthCalendar', async () => {
+    const page = await newSpecPage({
+      components: [HeaderCalendar],
+      template: ()=> (<header-calendar position="right"/>),
+    });
+    const component = page.rootInstance as HeaderCalendar;
+    const emitSpy = jest.spyOn(component.nextMonthCalendar, 'emit');
+    const button = page.root.shadowRoot.querySelector('.button-next') as HTMLElement;
+    button.click();
+    expect(emitSpy).toHaveBeenCalled();
+  });
+ 
 });
